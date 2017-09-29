06920c5908878702d8f5e9707a078ae3e5d2699b
Chris Dascoli <chris.dascoli@gmail.com>
2017-09-29 13:55:25 -0400

    updated baseurl

--- services/passport.js
+++ services/passport.js
@@ -20,7 +20,7 @@ passport.use(
   new GoogleStrategy({
     clientID: keys.googleClientID,
     clientSecret: keys.googleClientSecret,
-    callbackURL: '/auth/google/callback'
+    callbackURL: `${keys.baseURL}/auth/google/callback`
   }, (accessToken, refreshToken, profile, done) => {
     User.findOne({ googleID: profile.id })
       .then((existingUser) => {
