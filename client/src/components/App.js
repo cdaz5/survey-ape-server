98ce6718bdc06d7a275698a088f96124b63ea4f4
Chris Dascoli <chris.dascoli@gmail.com>
2017-10-07 08:54:27 -0400

    built out header component

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -5,7 +5,7 @@ import * as actions from '../actions';
 
 import Header from './Header'
 
-const Dashboard = () => <h2>Dashboard</h2>
+const Dashboard = () => <h2>Dashboard /surveys</h2>
 const SurveyNew = () => <h2>SurveyNew</h2>
 const Landing = () => <h2>Landing</h2>
 
