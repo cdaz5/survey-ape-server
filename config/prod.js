cdd8a676997ad09ecc31cde93c634c51356347fe
Chris Dascoli <chris.dascoli@gmail.com>
2017-10-09 10:53:20 -0400

    added stripe keys

--- config/prod.js
+++ config/prod.js
@@ -3,5 +3,7 @@ module.exports = {
   googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
   mongoURI: process.env.MONGO_URI,
   cookieKey: process.env.COOKIE_KEY,
-  baseURL: process.env.BASE_URL
+  baseURL: process.env.BASE_URL,
+  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY
 }
