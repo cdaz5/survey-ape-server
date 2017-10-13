4480ee2c2756eab6c91bb90693cae813feaa058d
Chris Dascoli <chris.dascoli@gmail.com>
2017-10-13 10:07:08 -0400

    initial setup of sendgrid in process

--- config/prod.js
+++ config/prod.js
@@ -5,5 +5,6 @@ module.exports = {
   cookieKey: process.env.COOKIE_KEY,
   baseURL: process.env.BASE_URL,
   stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
-  stripeSecretKey: process.env.STRIPE_SECRET_KEY
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY,
+  sendGridKey: process.env.SEND_GRID_KEY
 }
