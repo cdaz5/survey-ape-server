e675326cffa10cb48f714976008f4fa5474475c6
Chris Dascoli <chris.dascoli@gmail.com>
2017-10-13 13:26:17 -0400

    completed backend survey creating and emails send 2 more routes and webhook todos

--- routes/surveyRoutes.js
+++ routes/surveyRoutes.js
@@ -1,10 +1,17 @@
 const mongoose = require('mongoose');
 const requireLogin = require('../middlewares/requireLogin');
 const requireCredits = require('../middlewares/requireCredits');
+const Mailer = require('../services/Mailer');
+const surveyTemplate = require('../services/emailTemplates/surveyTemplate')
 const Survey = mongoose.model('surveys');
 
 module.exports = (app) => {
-  app.post('/api/surveys', requireLogin, requireCredits, (req, res) => {
+
+  app.get('/api/surveys/thanks', (req, res) => {
+    res.send('Thank you for your feedback!')
+  })
+
+  app.post('/api/surveys', requireLogin, requireCredits, async (req, res) => {
     let { title, subject, body, recipients } = req.body
 
     let survey = new Survey({
@@ -15,7 +22,17 @@ module.exports = (app) => {
       _user: req.user.id,
       dateSent: Date.now()
     })
-    
 
+    let mailer = new Mailer(survey, surveyTemplate(survey))
+    try {
+      await mailer.send()
+      await survey.save()
+      req.user.credits -= 1
+      let user = await req.user.save()
+
+      res.send(user)
+    } catch (err) {
+      res.status(422).send(err)
+    }
   })
 }
